19e1e7e9d041ab3adc896de920d7e9113999b0e3
Devan Shepard <wdevanshepard@gmail.com>
2024-12-04 21:15:06 -0600

    Add files via upload

--- CALI/7-launchtemplate.tf
+++ CALI/7-launchtemplate.tf
@@ -3,7 +3,7 @@ resource "aws_launch_template" "cali_LT" {
   image_id      = "ami-038bba9a164eb3dc1"  
   instance_type = "t2.micro"
 
-  key_name = "linuxbox"
+  # key_name = "linuxbox"
 
   vpc_security_group_ids = [aws_security_group.app1-sg01-servers.id]
 
