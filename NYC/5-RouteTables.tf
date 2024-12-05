7c3c87c5f5cdce14badfd8d456d301e9207b4319
Devan Shepard <wdevanshepard@gmail.com>
2024-12-04 21:17:28 -0600

    Add files via upload

--- NYC/5-RouteTables.tf
+++ NYC/5-RouteTables.tf
@@ -24,16 +24,22 @@ resource "aws_route_table" "private" {
   }
 }
 
-resource "aws_route_table_association" "public" {
+resource "aws_route_table_association" "public-us-east-1a" {
   subnet_id      = aws_subnet.public-us-east-1a.id
   route_table_id = aws_route_table.public.id
 }
-
-resource "aws_route_table_association" "private" {
+resource "aws_route_table_association" "public-us-east-1b" {
+  subnet_id      = aws_subnet.public-us-east-1b.id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "private-us-east-1a" {
   subnet_id      = aws_subnet.private-us-east-1a.id
   route_table_id = aws_route_table.private.id
 }
-
+resource "aws_route_table_association" "private-us-east-1b" {
+  subnet_id      = aws_subnet.private-us-east-1b.id
+  route_table_id = aws_route_table.private.id
+}
 # Add routes to the transit gateway
 # resource "aws_route" "tgw_routes" {
 #   route_table_id = aws_route_table.public.id
--- NYC/7-launchtemplate.tf
+++ NYC/7-launchtemplate.tf
@@ -3,7 +3,7 @@ resource "aws_launch_template" "nyc_LT" {
   image_id      = "ami-0453ec754f44f9a4a"  
   instance_type = "t2.micro"
 
-  key_name = "linuxbox"
+  #key_name = "linuxbox"
 
   vpc_security_group_ids = [aws_security_group.app1-sg01-servers.id]
 
