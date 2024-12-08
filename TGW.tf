add8627bf928e28bdc866a3c36b6ac8f54def77e
Devan Shepard <wdevanshepard@gmail.com>
2024-12-07 18:15:56 -0600

    Update TGW.tf

--- TGW.tf
+++ TGW.tf
@@ -16,7 +16,7 @@ resource "aws_ec2_transit_gateway" "transit_gateway-nyc" {
 #Create peering attachments between Tokyo, Hong Kong, Australia, and California
 resource "aws_ec2_transit_gateway_peering_attachment" "tgw-jp-hk-attachment" {
   provider = aws.tokyo
-  peer_account_id = "061039804531"
+  peer_account_id = "<enter account ID>"
   peer_region = "ap-east-1"
   peer_transit_gateway_id = aws_ec2_transit_gateway.transit_gateway-hk.id
   transit_gateway_id = aws_ec2_transit_gateway.transit_gateway-jp.id
@@ -26,7 +26,7 @@ resource "aws_ec2_transit_gateway_peering_attachment" "tgw-jp-hk-attachment" {
 }
 resource "aws_ec2_transit_gateway_peering_attachment" "tgw-jp-au-attachment" {
   provider = aws.tokyo
-  peer_account_id = "061039804531"
+  peer_account_id = "<enter account ID>"
   peer_region = "ap-southeast-2"
   peer_transit_gateway_id = "tgw-jp-au-attachment"
     transit_gateway_id = aws_ec2_transit_gateway.transit_gateway-jp.id
@@ -36,7 +36,7 @@ resource "aws_ec2_transit_gateway_peering_attachment" "tgw-jp-au-attachment" {
 }
 resource "aws_ec2_transit_gateway_peering_attachment" "tgw-jp-ca-attachment" {
   provider = aws.tokyo
-  peer_account_id = "061039804531"
+  peer_account_id = "<enter account ID>"
   peer_region = "us-west-1"
   peer_transit_gateway_id = "tgw-0a1b2c3d4e5f6g7h8"
   transit_gateway_id = aws_ec2_transit_gateway.transit_gateway-jp.id
@@ -47,7 +47,7 @@ resource "aws_ec2_transit_gateway_peering_attachment" "tgw-jp-ca-attachment" {
 #Create peering attachments between NYC, California, London, and Brazil
 resource "aws_ec2_transit_gateway_peering_attachment" "tgw-nyc-ca-attachment" {
   provider = aws.california
-  peer_account_id = "061039804531"
+  peer_account_id = ""
   peer_region = "us-east-1"
   peer_transit_gateway_id = aws_ec2_transit_gateway.transit_gateway-nyc.id
   transit_gateway_id = aws_ec2_transit_gateway.transit_gateway-nyc.id
@@ -57,7 +57,7 @@ resource "aws_ec2_transit_gateway_peering_attachment" "tgw-nyc-ca-attachment" {
 }
 resource "aws_ec2_transit_gateway_peering_attachment" "tgw-nyc-london-attachment" {
   provider = aws.nyc
-  peer_account_id = "061039804531"
+  peer_account_id = ""
   peer_region = "eu-west-2"
   peer_transit_gateway_id = "tgw-nyc-london-attachment"
   transit_gateway_id = aws_ec2_transit_gateway.transit_gateway-nyc.id
